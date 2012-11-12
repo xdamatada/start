e591dddfe75b51170072ab8e1804cf18e1a58b17
Matada <XDAMatada@gmail.com>
2012-11-12 12:36:22 -0600

    fixed wrong name in gitupdate.sh

--- gitupdate.sh
+++ gitupdate.sh
@@ -7,7 +7,7 @@ GREP="/bin/grep"
 #....
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root. Rerun using sudo ./Dirtyroomcooker.sh" 1>&2
+   echo "This script must be run as root. Rerun using sudo ./start.sh" 1>&2
    exit 1
 fi
 
